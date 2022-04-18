e91a8b6643ee5b10b4594bc78c83d4c87b1592ac
Martin Roux <martinrouxpereyra@hotmail.com>
2022-04-18 14:12:24 -0300

    BackEnd basico listo

---
+++ controllers/user.js
@@ -0,0 +1,64 @@
+const {response, request} = require('express');
+
+const usuariosGet = (req = request , res = response) => {
+
+    const {q, nombre = 'No name', apikey, page = 1, limit} = req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    });
+}
+
+const usuariosPost = (req, res) => {
+
+    const {nombre, edad} = req.body;
+
+    res.status(201).json({
+
+        msg: 'post API - controlador',
+        nombre,
+        edad
+    });
+}
+
+const usuariosPut = (req, res) => {
+
+    const { id } = req.params;   
+
+    res.status(400).json({
+        msg: 'put API - controlador',
+        id
+    });
+    
+}
+
+const usuariosPatch = (req, res) => {
+
+    res.json({
+        msg: 'patch API - controlador'
+    });
+    
+}
+
+const usuariosDelete = (req, res) => {
+
+    res.json({
+        msg: 'delete API - controlador'
+    });
+}
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete
+}
+
+
---
+++ models/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('../routes/user');
+
+class Server{
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.port;
+        this.usuariosPath = '/api/usuarios';
+        //Middlewares
+        this.middlewares();
+
+        //rutas de mi aplicacion
+        this.routes();
+    }
+
+    middlewares(){
+
+        //cors
+        this.app.use(cors());
+
+        //lectura y parseo del body   
+        this.app.use(express.json());
+        //directorio publico
+        this.app.use(express.static('public') );
+
+
+    }
+
+    routes(){
+
+       this.app.use(this.usuariosPath, require('../routes/user')); 
+    }
+
+    listen(){
+        this.app.listen(this.port, () =>{
+            console.log('listening on port', this.port);
+        });
+    }
+}
+
+module.exports = Server;
